0bd4f0845ba8941996e16f5eb6f084824f1019a4
HAMZ00 <35032582+HAMZ00@users.noreply.github.com>
2018-01-28 23:29:10 +0500

    Add files via upload

---
+++ ASSIGNMENT1part3.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID As Integer = 0
+        Dim Name As String = " "
+        Dim FindName As String = " "
+        Dim FindID As Integer = " "
+        Dim found As Boolean = False
+
+        FileOpen(1, "E:\hamza\vb\assignment1.txt", OpenMode.Input)
+
+        Console.WriteLine("Enter name to be searched: ")
+        FindName = FindName.ToLower()
+
+        Do
+            Input(1, Name)
+            Input(1, MemberID)
+
+            Name = Name.ToLower
+
+            If FindName = Name Then
+                found = True
+                Console.WriteLine("member name: ")
+                Console.WriteLine(MemberID)
+                Console.Write("member id : ")
+                Console.WriteLine(FindID)
+            Else
+                found = False
+            End If
+        Loop Until Not EOF(1) Or found = True
+
+        If found = False Then
+            Console.WriteLine(" RECORD NOT FOUND")
+
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
