d7ecab88e77c4106876a40a17162c9daad6ae0b5
HAMZ00 <35032582+HAMZ00@users.noreply.github.com>
2018-01-28 22:40:12 +0500

    Add files via upload

---
+++ assignment1part2.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID As Integer = 0
+        Dim Name As String = " "
+
+        FileOpen(1, "E:\hamza\vb\assignment1.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, Name)
+            Input(1, MemberID)
+            Console.WriteLine(Name)
+            Console.WriteLine(MemberID)
+            Console.WriteLine(" ")
+        End While
+        Console.ReadKey()
+
+    End Sub
+
+End Module
